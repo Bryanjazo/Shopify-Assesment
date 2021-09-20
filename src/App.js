7500ebab70aba28d2ee104bece5ef4a4ac24f888
Bryan Jazo <bryanjazo88@gmail.com>
2021-09-20 13:08:52 -0700

    Set Loading Screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import Home from './Components/Home'
 import NavBar from './Components/Navbar/Navbar'
-
+import {useDispatch,useSelector} from 'react-redux'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-
+import { AutoFixOffSharp } from '@mui/icons-material';
+import {getMarsRoverDetails} from './Redux/nasaReducer'
+import { css } from "@emotion/react";
+import GridLoader from "react-spinners/GridLoader";
+import * as ReactBootStrap from 'react-bootstrap'
 function App() {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  console.log(loading, 'loading')
+  const override = css`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+`;
+  const loadingFunction = async() =>{
+    try {
+      setLoading(true)
+      const data = await dispatch(getMarsRoverDetails())
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadingFunction()
+  }, [])
   return (
   <Router>
+
     <div className="App">
+    
       <NavBar />
+     
        <Switch>
+     
           <Route path="/">
-            <Home />
+        {loading === false ? <Home /> : <GridLoader css={override} color={'#6854fc'} loading={loading} size={50} />}
           </Route>
         </Switch>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,11 @@
 import React, {useEffect} from 'react'
-import {getMarsRoverDetails} from '../Redux/nasaReducer'
+
 import {useDispatch,useSelector} from 'react-redux'
 import Pictures from './Pictures'
 
 export default function Home() {
     const {nasaDetails} = useSelector((state) => state.nasa)
-    const dispatch = useDispatch()
-    useEffect(() => {    
-       dispatch(getMarsRoverDetails())
-    },[dispatch])
+  
 
     
     let pictures = nasaDetails.map(details => details.photos.slice(0,100).map(marsDetails => <Pictures {...marsDetails}/>))
